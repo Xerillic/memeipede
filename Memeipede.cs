d9dbd50600227096f0d66fedc1bdebb581d53379
Xerillic <millsleedamien01@gmail.com>
2023-02-06 16:38:27 -0500

    alright this is done for now

--- Memeipede.cs
+++ Memeipede.cs
@@ -31,29 +31,33 @@ namespace Memeipede
                 if (behave != centi.AI.behavior)
                 {
                     behave = centi.AI.behavior;
-
-                    for (int i = 0; i < centi.room.game.cameras[0].virtualMicrophone.soundObjects.Count; i++)
-                    {
-                        if (centi.room.game.cameras[0].virtualMicrophone.soundObjects[i].soundData.soundName.Contains("memeipede"))
-                        {
-                            centi.room.game.cameras[0].virtualMicrophone.soundObjects[i].Destroy();
-                        }
-                    }
                     PlaySound(behave);
                 }
             }
         }
-
         private void PlaySound(CentipedeAI.Behavior behavior)
         {
-            // Might add more
+            currentsound?.Destroy();
             if (behavior == CentipedeAI.Behavior.Hunt)
             {
                 centi.room.PlayCustomChunkSound("memeipede_hunt", centi.mainBodyChunk, 1f, 1f);
             }
+
+            // More behaviors later maybe
+
+            for (int i = 0; i < centi.room.game.cameras[0].virtualMicrophone.soundObjects.Count; i++)
+            {
+                if (centi.room.game.cameras[0].virtualMicrophone.soundObjects[i].soundData.soundName.Contains("memeipede"))
+                {
+                    currentsound = centi.room.game.cameras[0].virtualMicrophone.soundObjects[i];
+                }
+            }
+            currentsound.soundData.dopplerFac = 0f;
         }
 
+        public VirtualMicrophone.SoundObject currentsound;
         public CentipedeAI.Behavior behave;
         public Centipede centi;
+        public bool move;
     }
 }
